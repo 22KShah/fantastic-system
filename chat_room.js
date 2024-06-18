6d44e40b1a632e13cd9448cfd5321b4f570ea2ce
22KShah <116306212+22KShah@users.noreply.github.com>
2024-06-18 19:10:18 +0100

    Add files via upload

---
+++ chat_room.js
@@ -0,0 +1,14 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAJ9OL3HNlsLFnQb3DNV1m_GwtC83dKwLk",
+    authDomain: "lets-chat-webapp-ffb7e.firebaseapp.com",
+    databaseURL: "https://lets-chat-webapp-ffb7e-default-rtdb.firebaseio.com",
+    projectId: "lets-chat-webapp-ffb7e",
+    storageBucket: "lets-chat-webapp-ffb7e.appspot.com",
+    messagingSenderId: "936311143909",
+    appId: "1:936311143909:web:a9906312dd5a9219a0430c"
+  };
+
+
+// Initialize Firebase
+
+firebase.initializeApp(firebaseConfig);
